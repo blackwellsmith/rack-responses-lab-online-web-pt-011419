777b48b96717a12c807bff69b372355a5e8ca555
blackwell <blackwellsmithiv@gmail.com>
2019-04-08 18:35:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,11 @@
+require 'pry'
 class Application
 
   def call(env)
     resp = Rack::Response.new
     
     time = Time.hour.now
-    
+    binding.pry
     if time >= 12
       resp.write "Good Afternoon!"
     else
