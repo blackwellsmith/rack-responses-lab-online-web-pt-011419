da42664a70699816512df6a4bf16d38abf438acd
blackwell <blackwellsmithiv@gmail.com>
2019-04-08 18:24:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    if Time >= 12
+    if Time.new >= 12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning"
