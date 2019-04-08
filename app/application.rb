5a46d9d7678da81914744888afee8ccf701a0296
blackwell <blackwellsmithiv@gmail.com>
2019-04-08 18:20:12 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Time
+
+  def call(env)
+    resp = Rack::Response.new
+    
+    time = Kernal.rand(1..24)
+    
+    if time >= 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning"
+    end
+    resp.finish
+  end
+end
